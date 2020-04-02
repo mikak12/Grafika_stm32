a7b06d0f5185fee2fd388f3348a9135a9065c7f8
mikak12 <starzyk.dominik@gmail.com>
2020-04-02 15:41:26 +0200

    program do modyfikowania wyswietlanej grafiki na stm32f429zi
    Signed-off-by: mikak12 <starzyk.dominik@gmail.com>

---
+++ Dziala_grafika/Objects/guiconf.d
@@ -0,0 +1,9 @@
+.\objects\guiconf.o: RTE\Graphics\GUIConf.c
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\GUI.h
+.\objects\guiconf.o: E:\instalki\KeilInstall\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\GUI_ConfDefaults.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\GUIConf.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\GUI_Type.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\LCD.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\Global.h
+.\objects\guiconf.o: E:\instalki\KeilPack\Keil\MDK-Middleware\7.10.0\emWin\Include\GUI_Version.h
---
+++ Dziala_grafika/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,42 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Pierwszy' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_DMA2D
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_LTDC
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SPI
+#define RTE_DEVICE_STARTUP_STM32F4XX    /* Device Startup for STM32F4 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+        #define RTE_Drivers_SPI5                /* Driver SPI5 */
+        #define RTE_Drivers_SPI6                /* Driver SPI6 */
+#define RTE_Graphics_Core               /* Graphics Core */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+
+#endif /* RTE_COMPONENTS_H */
